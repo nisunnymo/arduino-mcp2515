9437cfe45c37cf437f357460c5f613f6f2ccf154
ptoshkov <55318963+ptoshkov@users.noreply.github.com>
2025-08-31 18:25:11 +0300

    Update can.h

--- can.h
+++ can.h
@@ -37,9 +37,9 @@ typedef __u32 canid_t;
 #define CAN_MAX_DLEN 8
 
 struct can_frame {
-    canid_t can_id;  /* 32 bit CAN_ID + EFF/RTR/ERR flags */
-    __u8    can_dlc; /* frame payload length in byte (0 .. CAN_MAX_DLEN) */
-    __u8    data[CAN_MAX_DLEN] __attribute__((aligned(8)));
+               canid_t can_id;  /* 32 bit CAN_ID + EFF/RTR/ERR flags */
+               __u8    can_dlc; /* frame payload length in byte (0 .. CAN_MAX_DLEN) */
+    alignas(8) __u8    data[CAN_MAX_DLEN];
 };
 
 #endif /* CAN_H_ */
